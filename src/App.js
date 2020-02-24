ba4d9fdf9c907af22e20c64e258c93c77be71e45
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-02-24 03:54:17 -0800

    add food.js logic

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import Snake from './Snake';
+import Food from './Food';
 
 class App extends Component{
 
   state= {
+    food: [6,8],
     snakeDots: [
       [0,0],
       [2,0]
@@ -13,6 +15,7 @@ class App extends Component{
   return (
     <div className="game-area">
       <Snake snakeDots={this.state.snakeDots} />
+      <Food dot={this.state.food} />
     </div>
   );
 }
