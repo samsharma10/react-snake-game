cf1443c713de36759f3ea165fcac1f85deb6c486
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-02-24 03:43:34 -0800

    add div for snake-dots to app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
 class App extends Component{
+
+  state= {
+    snakeDots: [
+      [0,0],
+      [2,0]
+    ]
+  }
   render(){
   return (
-    <div className="game-area"></div>
+    <div className="game-area">
+      <div className="snake-dot" style={{top:0, left:0}}></div>
+      <div className="snake-dot" style={{top:0, left:2%}}></div>
+      <div className="snake-dot" style={{top:0, left:4%}}></div>
+    </div>
   );
 }
 }
