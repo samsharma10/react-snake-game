6bddef45e700709ab574ea9380ef694fca19d607
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-02-24 03:50:12 -0800

    add snake.js logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Snake from './Snake';
 
 class App extends Component{
 
@@ -11,9 +12,7 @@ class App extends Component{
   render(){
   return (
     <div className="game-area">
-      <div className="snake-dot" style={{top:0, left:0}}></div>
-      <div className="snake-dot" style={{top:0, left:2%}}></div>
-      <div className="snake-dot" style={{top:0, left:4%}}></div>
+      <Snake snakeDots={this.state.snakeDots} />
     </div>
   );
 }
