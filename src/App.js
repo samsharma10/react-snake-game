ff94655bf1d69b53a7d37cca2f3670c08ed233f7
Sam Sharma <samsharma@Sams-MacBook-Pro.local>
2020-02-24 04:25:28 -0800

    add checkIfCollapsed method

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ class App extends Component{
 
   componentDidUpdate(){
     this.checkIfOutOfBorders();
+    this.checkIfCollapsed();
   }
 
   onkeydown = (e) => {
@@ -81,6 +82,17 @@ checkIfOutOfBorders(){
   if(head[0] >= 100 || head[1] >= 100 || head[0] < 0 || head[1]<0){
     this.onGameOver();
   }
+}
+
+checkIfCollapsed(){
+  let snake = [...this.state.snakeDots];
+  let head = snake[snake.length -1];
+  snake.pop();
+  snake.forEach(dot=> {
+    if(head[0] == dot[0] && head[1] == dot[1]){
+      this.onGameOver();
+    }
+  })
 }
 
 onGameOver(){
